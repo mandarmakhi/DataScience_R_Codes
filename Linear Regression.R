4a367cf42c58f171092ec94202f9bb74d0b7c38e
Mandar Makhi <43196744+mandarmakhi@users.noreply.github.com>
2020-02-21 13:53:15 +0530

    Update Linear Regression.R

--- Linear Regression.R
+++ Linear Regression.R
@@ -7,6 +7,8 @@ pred
 
 #load data
 Nd<-NewspaperData.csv("C:\Users\Mandar\Desktop\excelr\NewspaperData.csv")
+
+
 #Visulaization
 library("lattice")
 dotplot(NewspaperData$sunday, main="Dot Plot of sunday Circulation", col="dodgerblue4")
@@ -17,9 +19,17 @@ boxplot(NewspaperData,col="red")
 
 #Regression equation 
 #syntax model<-lm(y~x,data=dat set name)
+#finding Y
 model<- lm(sunday~daily,data=NewspaperData)
 summary(model)
+
+
+#finding Y^2 pridication 
 new<-data.frame(daily=c(200,250,300))
 pred<-predict(model,newdata=new)
+
+
+
+#finding error
 pred<-predict(model)
 finaldat<- data.frame(NewspaperData,pred,"ERROR"=NewspaperData$sunday-pred)
