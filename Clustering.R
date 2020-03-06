28916488245dec6357b15b92ea800565a4dfc2b1
Mandar Makhi <43196744+mandarmakhi@users.noreply.github.com>
2020-03-06 13:07:09 +0530

    Update and rename Clustering(university data).R to Clustering.R

--- Clustering.R
+++ Clustering.R
@@ -9,8 +9,11 @@ d<- dist(universities,method="euclidean")#Computing the distance matrix
 fit<- hclust(d,method ="average")#Building the algorithm for average '
 plot(fit)#display dendogram
 groups<-cutree(fit,k=4)#cut three into 4 clusters
+
+
 #draw dendogram with red border aroound the 5 cluster
 rect.hclust(fit,k=4,border ="red")
+
 #attach the cluster number to uni
 clusters=data.frame('Uni'=universities[,1],'Cluster'=groups)
 
